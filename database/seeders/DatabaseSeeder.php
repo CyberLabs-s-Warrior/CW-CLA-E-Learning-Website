54d46713670345fd8736436fef66bd27a428b9d8
arya wirasatya <aryawirasatya39@gmail.com>
2025-07-30 11:44:21 +0700

    fix: akses permission penuh pada superadmin di seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,31 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Database\Seeders\PermissionSeeder as SeedersPermissionSeeder;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use PermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => bcrypt('password123'), 
-        // ]);
         $this->call([
             RoleSeeder::class,
+            PermissionSeeder::class, 
             SuperadminSeeder::class,
         ]);
-
-
     }
 }
